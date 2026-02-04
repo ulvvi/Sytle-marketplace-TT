2809837ae359313ad6679e043e4b33305a0b8dec
Taylor <taylorhorta@gmail.com>
2026-02-04 19:51:15 -0300

    refactor: rotas teste comentadas

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { UserController } from "../controllers/UserController";
 const router = Router();
-
+/*
 router.post("/signUp", UserController.SignUp);
 router.get("/user/:id", UserController.readUser);
 router.get("/users", UserController.readAllUsers);
 router.put("/user/:id", UserController.updateUser);
 router.delete("/user/:id", UserController.deleteUser);
+*/
+
+export default router
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,4 @@
+import routes from '../src/routes/routes';
 import express from 'express';
 import configDotenv from './config/dotenv';
 // import cors from 'cors';
@@ -11,7 +12,7 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //app.use(cors());
-//app.use(routes);
+app.use(routes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
