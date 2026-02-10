a682093a79f9087e468051efa639139d9c1b94e6
Thiago Ferraco de Andrade <thiago_ferrasso@ufrj.br>
2026-02-10 13:31:52 -0300

    refactor: Otimização das Componentes
    Foi necessário mudanças para que essas componentes sejam usadas em outras telas.

--- frontend/src/components/InputText.tsx
+++ frontend/src/components/InputText.tsx
@@ -5,14 +5,16 @@ interface InputTextProps {
     icone?: ReactNode;
     texto?: string;
     isPassword?: boolean;
-    label?: string;     
+    label?: string;
+    inputClassName?: string;
 }
 
 export function InputText({
-    icone = <div className="w-6 h-6 bg-gray-200 rounded-full" />,
+    icone,
     texto = "Enter your text",
     isPassword = false,
-    label = "Field Name"
+    label,
+    inputClassName
 }: InputTextProps) {
 
     const [isClicked, setIsClicked] = useState(false);
@@ -28,11 +30,11 @@ export function InputText({
 
     return (
         <div className="w-full gap-[12px] flex flex-col items-start justify-center">
-            <label className="text-[14px] text-primary font-medium block">
+            <label className={`text-[14px] text-primary font-medium ${label === undefined ? "hidden" : "block"}`}>
                 {label}
             </label>
                 
-            <div className="flex items-center justify-evenly border-(--border-primary) rounded-[10px] border-[1px] border-solid w-full h-[40px] gap-[13px] px-[12px]">
+            <div className={`flex items-center justify-evenly border-(--border-primary) rounded-[10px] border-[1px] border-solid w-full h-[40px] gap-[13px] px-[12px] ${inputClassName}`}>
                 {renderIcone()}
 
                 <input 
