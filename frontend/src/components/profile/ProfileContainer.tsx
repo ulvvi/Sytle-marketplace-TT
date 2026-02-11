fb68ab3cc347402ecf4d9301287cf0265cf20293
Thiago Ferraco de Andrade <thiago_ferrasso@ufrj.br>
2026-02-11 18:51:27 -0300

    feature: ProfileContainer COMPONENT
    Componente abaixo do cardname (Segundo principal)

---
+++ frontend/src/components/profile/ProfileContainer.tsx
@@ -0,0 +1,15 @@
+import { TabList } from "./TabList";
+
+export function ProfileContainer (){
+    return(
+        <>
+        <div className="w-full gap-[8px]">
+        <TabList/>
+        
+        </div>
+        
+        
+        
+        </>
+    )
+}
