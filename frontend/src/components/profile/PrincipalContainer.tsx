112a862423242e50796c32c776274d61f3f5eee2
Thiago Ferraco de Andrade <thiago_ferrasso@ufrj.br>
2026-02-11 18:42:35 -0300

    feature: PrincipalConteiner COMPONENT
    Conteiner principal da página

---
+++ frontend/src/components/profile/PrincipalContainer.tsx
@@ -0,0 +1,15 @@
+import { TabList } from "./TabList";
+
+export function PrincipalContainer (){
+    return(
+        <>
+        <div className="w-full gap-[8px]">
+        <TabList/>
+        
+        </div>
+        
+        
+        
+        </>
+    )
+}
