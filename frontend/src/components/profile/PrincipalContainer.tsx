174ac896201d5d4a49c0171dfd1e752cf523a2b8
Thiago Ferraco de Andrade <thiago_ferrasso@ufrj.br>
2026-02-11 20:45:55 -0300

    refactor: troca dos nomes dos componentes

--- frontend/src/components/profile/PrincipalContainer.tsx
+++ frontend/src/components/profile/PrincipalContainer.tsx
@@ -6,8 +6,8 @@ export function PrincipalContainer(){
     return(
         <>
             <div className="flex flex-col items-center justify-start gap-[32px] mt-[32px] pr-[16px] mb-[32px] pl-[16px]">
-            <PersonCardName name={"Jhon Doe"} email={"john.doe@example.com"}/>
-            <PrincipalContainer/>
+            <PersonCardName name={"Jhon Doe"} email={"john.doe@example.com"} orders={2} memberSince={2023}/>
+            <ProfileContainer/>
             
 
             </div>
