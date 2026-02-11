d769d074a87ac198fa8d3bd9971ba709429bb213
Thiago Ferraco de Andrade <thiago_ferrasso@ufrj.br>
2026-02-11 18:52:37 -0300

    feature: Pagina principal com header

---
+++ frontend/src/pages/Profile.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../components/Header"
+import { PrincipalContainer } from "../components/profile/PrincipalContainer"
+
+export function Profile() {
+    return (
+        <>
+            <Header/>
+            <PrincipalContainer/>
+        </>
+    )
+}
