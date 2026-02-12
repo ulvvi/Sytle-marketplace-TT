04fdd0806250ac8d845dee92109cf4df4d0e11cc
Taylor <taylorhorta@gmail.com>
2026-02-12 12:03:25 -0300

    feature: component product section(inacabado)

---
+++ frontend/src/components/Sales/ProductSection.tsx
@@ -0,0 +1,19 @@
+import { ProductCard } from "../ProductCard"
+
+export function ProductSection(){
+    return(
+        <div>
+            {/**div de sale items  */}
+            <div>
+                
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </div>
+            <ProductCard/>
+        </div>
+    )
+}
