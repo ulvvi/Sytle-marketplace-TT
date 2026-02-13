8418bdf21a71c77a2a72157c3fad5b00177ae3d6
Taylor <taylorhorta@gmail.com>
2026-02-13 02:49:27 -0300

    bugfix: svg icon component hidden na tela home

--- frontend/src/components/SvgIconProduct.tsx
+++ frontend/src/components/SvgIconProduct.tsx
@@ -1,10 +1,11 @@
 interface SvgProps{
     path: string,
     alt: string,
-    border: string
+    border: string,
+    className: string
 }
-export function SvgIconProduct({path, alt, border} : SvgProps){
+export function SvgIconProduct({path, alt, border, className} : SvgProps){
     return(
-        <img src={path} alt={alt} className={`${border === 'true' ? 'border border-(--border-primary)' : ''} rounded-[0.65rem] px-2.5 `}/>
+        <img src={path} alt={alt} className={`${border === 'true' ? 'border border-(--border-primary)' : ''} rounded-[0.65rem] px-2.5 ${className}`}/>
     )
 }
