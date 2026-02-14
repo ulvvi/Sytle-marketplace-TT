c6de6cc35fa42a76275394793b2b98ae2523970f
Taylor <taylorhorta@gmail.com>
2026-02-13 23:33:07 -0300

    bugfix: ajuste no padding

--- frontend/src/components/SvgIconProduct.tsx
+++ frontend/src/components/SvgIconProduct.tsx
@@ -2,10 +2,13 @@ interface SvgProps{
     path: string,
     alt: string,
     border: string,
-    className: string
+    className?: string
+    color?: string
 }
-export function SvgIconProduct({path, alt, border, className} : SvgProps){
+export function SvgIconProduct({path, alt, border, className, color} : SvgProps){
     return(
-        <img src={path} alt={alt} className={`${border === 'true' ? 'border border-(--border-primary)' : ''} rounded-[0.65rem] px-2.5 ${className}`}/>
+        <button>
+            <img src={path} alt={alt} className={`${border === 'true' ? 'border border-(--border-primary)' : ''} rounded-[0.65rem] p-2.5 ${className} ${color}`}/>
+        </button>
     )
 }
