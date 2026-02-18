41a20ed3e3bd748647366b085be82c8bfd56b5eb
Taylor <taylorhorta@gmail.com>
2026-02-18 14:09:18 -0300

    feature: criacao do user context

---
+++ frontend/src/contexts/userContext.ts
@@ -0,0 +1,20 @@
+import {createContext } from "react";
+
+export type userInfo = {
+    firstName: string,
+    lastName: string,
+    email: string,
+    gender?: "FEMALE" | "MALE" | "OTHER",
+    phoneNumber?: string,
+    dateBirth?: Date,
+    totalOrders: number,
+    totalRating: number,
+    totalWishlist: number,
+    emailNotification: boolean,
+    smsNotification: boolean,
+    marketingEmail: boolean,
+    orderUpdate: boolean,
+    newArrival: boolean,
+    saleAlert: boolean,
+}
+export const userContext = createContext<userInfo | undefined>(undefined);
