4093755e0791063b23952d7cbb71b1b8ae4b1afe
contilou <luiz.afconti@gmail.com>
2026-02-19 19:51:17 -0300

    feature: adicionar na wishlist

--- frontend/src/components/productInfo/ProductGallery.tsx
+++ frontend/src/components/productInfo/ProductGallery.tsx
@@ -2,6 +2,7 @@ import { useContext, useState } from "react";
 import { IconButton } from "../IconButton";
 import { SvgIconProduct } from "../SvgIconProduct";
 import { ProductInfoContext } from "../../pages/ProductInfo";
+import { UserContext } from "../../contexts/UserProvider";
 
 interface ProductGalleryProps {
     productPics?: ProductPic[];
@@ -17,13 +18,51 @@ export function ProductGallery({productPics = new Array(4).fill({imgSrc: "/src/a
     const isFirstImage = currentImage === 0;
     const isLastImage = currentImage === productPics.length-1;
     const product = useContext(ProductInfoContext)
+    const {user, isLogged} = useContext(UserContext)
 
     const [isLiked, setIsLiked] = useState(false)
     const likeProduct = () =>{
         setIsLiked(!isLiked)
     }
 
-        const discount = product?.SalePrice ? Math.round( (1 - (product.SalePrice/(product.price as number)))*100 ) : undefined; 
+    async function toggleLike(){
+        if (isLogged && product && user?.id){   
+            if (isLiked){
+                const response = await fetch(`http://localhost:3333/user/${user.id}/wishlist/del`, {
+                            method: "PUT",
+                            headers: {
+                            "Content-type": "application/json",
+                            "Authorization": `Bearer ${localStorage.getItem("styleToken")}`
+                            },
+                            body: JSON.stringify({
+                                productId: product.id
+                            })
+                })
+
+                if(response.ok){
+                    setIsLiked(false)
+                }
+            } else {
+                const response = await fetch(`http://localhost:3333/user/${user.id}/wishlist/add`, {
+                            method: "PUT",
+                            headers: {
+                            "Content-type": "application/json",
+                            "Authorization": `Bearer ${localStorage.getItem("styleToken")}`
+                            },
+                            body: JSON.stringify({
+                                productId: product.id
+                            })
+                })
+                if(response.ok){
+                    setIsLiked(true)
+                }
+            }
+        } else {
+            setIsLiked(false)
+        }
+    }
+
+    const discount = product?.SalePrice ? Math.round( (1 - (product.SalePrice/(product.price as number)))*100 ) : undefined; 
 
     return (
         <>
@@ -33,7 +72,7 @@ export function ProductGallery({productPics = new Array(4).fill({imgSrc: "/src/a
                         <span className={`text-secondary font-semibold text-[0.75rem]`}>-{discount}%</span>
                     </div>
                     <div className="absolute right-4 top-4">
-                        <SvgIconProduct color="bg-[#F3F4F6]" border="false" onClick={likeProduct} path={`${isLiked ?'/src/assets/icons/heartFilled.svg' : '/src/assets/icons/heartIcon.svg' }`} alt="Ícone para salvar na wishlist" className="hover:opacity-80"/>
+                        <SvgIconProduct color="bg-[#F3F4F6]" border="false" onClick={() => {toggleLike()}} path={`${isLiked ?'/src/assets/icons/heartFilled.svg' : '/src/assets/icons/heartIcon.svg' }`} alt="Ícone para salvar na wishlist" className="hover:opacity-80"/>
                     </div>
                     <IconButton iconSrc="/src/assets/icons/sliderArrowIcon.svg" onClick={() => isFirstImage ? "" : setCurrentImage(currentImage-1)} buttonClassName={`disabled:!cursor-not-allowed disabled:opacity-50 disabled:hover:bg-secondary bg-secondary !absolute left-4 top-1/2 -translate-y-1/2 scale-x-[-1]`} disabled={isFirstImage}/>
                     <img src={productPics[currentImage].imgSrc} alt={productPics[currentImage].alt} className="w-full" />
