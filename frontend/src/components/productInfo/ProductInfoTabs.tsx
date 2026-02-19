49e95c3d73a2b78409a6a9afa9a0099ef52ed7be
contilou <luiz.afconti@gmail.com>
2026-02-19 13:23:09 -0300

    feature: numero de reviews integrado

--- frontend/src/components/productInfo/ProductInfoTabs.tsx
+++ frontend/src/components/productInfo/ProductInfoTabs.tsx
@@ -1,4 +1,9 @@
+import { useContext } from "react"
+import { ProductInfoContext } from "../../pages/ProductInfo"
+
 export function ProductInfoTabs() {
+    const product = useContext(ProductInfoContext)
+    
     return ( 
         <>
 
@@ -11,7 +16,7 @@ export function ProductInfoTabs() {
                         Specifications
                     </button>
                     <button className={`bg-[#F3F4F6] font-semibold text-tertiary pt-1.5 pr-[10.5px] pb-1.5 pl-[18.5px] cursor-pointer rounded-lg text-[0.875rem] w-full` } >
-                        Reviews (124)
+                        Reviews ({product?.numOfReviews})
                     </button>
                 </nav>
                 <article className="flex flex-col gap-4.5 p-6 border border-(--border-primary) rounded-xl">
