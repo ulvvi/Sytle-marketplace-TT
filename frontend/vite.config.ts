bdea0a1b9f11861c18e9d01acbbff0b58c6274fb
Thiago Ferraco de Andrade <thiago_ferrasso@ufrj.br>
2026-02-19 23:14:32 -0300

    bugfix: mudança de proxy
    teve que mudar o proxy do Vite para evitar bug de CORS do navegador

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,13 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  //Talvez precise remover...
+  server: {
+    proxy: {
+      '/signup': {
+        target: 'http://localhost:3333',
+        changeOrigin: true,
+      }
+    }
+  }
 })
